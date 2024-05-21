cbcf36f39b7685912fdbeb914c783cf8a1360569
Veronika <xafling@mail.ru>
2024-05-21 14:24:52 +0300

    card desktop done

--- src/components/Main/Products/Products.jsx
+++ src/components/Main/Products/Products.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import cn from 'classnames';
 import s from './styles.module.scss';
+import cardProductList from "../../ui/CardProduct/CardProduct";
 
 export const Products = () => {
 
   return (
     <div className={s.products}>
       <div className={s.products__title}>Our products</div>
-      <div className={s.products__cards}>Cards</div>
+      <div className={s.products__cards}>
+        {cardProductList.map((card, index) => (
+          <div key={index} className={s.card}>
+            {card}
+          </div> 
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/ui/CardProduct/CardProduct.jsx
+++ src/components/ui/CardProduct/CardProduct.jsx
@@ -1,15 +1,42 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import cn from 'classnames';
 import s from './styles.module.scss';
 
-export const CardProduct = () => {
+import arrowToLink from '../../../assets/arrowToLink.svg';
+
+const titles = [
+  {color: "rgb(255, 200, 0)", title: "Ultimate Guitar", text: "The best platform for music makers to play songs they love. Home to the world's largest music community.", },
+  {color: "rgb(46, 104, 192)", title: "MuseScore", text: "Create, play back, and print sheet music with free open source notation software.", },
+  {color: "rgb(234, 21, 66)", title: "audio.com", text: "The easiest way to share any sound online.", },
+  {color: "rgb(29, 78, 216)", title: "Audacity", text: "The world's most popular audio editing and recording app.", }
+];
+
+export const CardProduct = ({color, title, text, ...props}) => {
 
   return (
     <div className={s.card}>
-        <div className={s.card__color}></div>
+        <div className={s.card__color} style={{ backgroundColor: color }}></div>
         <div className={s.card__info}>
-          
+          <div className={s.card__info_title}>{title}</div>
+          <div className={s.card__info_text}>{text}</div>
         </div>
+        <a className={s.card__link} href="/">
+          <div className={s.card__link__text}>Read more</div>
+          <img src={arrowToLink} alt={'arrow to link'}/>
+        </a>
     </div>
   )
 }
+
+CardProduct.propTypes = {
+  color: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+const cardProductList = titles.map((item, index) => (
+  <CardProduct key={index} {...item} />
+));
+
+export default cardProductList;
