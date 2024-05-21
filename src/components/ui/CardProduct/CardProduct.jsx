73fbb036108ce2f58623af4cab441468452306ba
Veronika <xafling@mail.ru>
2024-05-21 10:51:01 +0300

    started card

---
+++ src/components/ui/CardProduct/CardProduct.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import cn from 'classnames';
+import s from './styles.module.scss';
+
+export const CardProduct = () => {
+
+  return (
+    <div className={s.card}>
+        <div className={s.card__color}></div>
+        <div className={s.card__info}>
+          
+        </div>
+    </div>
+  )
+}
